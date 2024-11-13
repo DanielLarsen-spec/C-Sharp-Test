603690202507d88aebaa6bbfeb34c830d6dae89e
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-11-13 12:01:06 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+
+string text = "hello world";
+int number = 5;
+bool b = false;
+float f = 0.0f;
+double d = 5.5;
+char c = 'a';
+string s = "ss";
+
+Console.WriteLine(c);
+
+var text2 = "hello world";
+var number2 = 5;
+var b2 = false;
+var f2 = 0.0f;
+var d2 = 5.5;
+var c2 = 'a';
+var s2 = "ss";
+
+var x = 3;
+
+number2 = Convert.ToInt32(text2);
+Console.WriteLine(number2);
+
